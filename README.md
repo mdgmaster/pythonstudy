da372d5e3addf6f2f2f03b0610e96a2af3b36b16
mdg <sergiomadrigal@gmail.com>
2020-07-29 14:39:22 +0200

    Test Update 0.2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Python Study Project
 Proyecto de estudio de Python 
 
+## Version
+0.1: Inicio del proyecto
+0.2. Actualización de ejercicios
---
+++ guessing.py
@@ -0,0 +1,25 @@
+import random
+# Guessing Game v.1.0
+print("--------------------------------------")
+print("          M&M Guessing game!")
+print("--------------------------------------")
+
+mm_count = random.randint(1, 100)
+chances = 5
+while(chances > 0 ):
+    intento = int(input("Di tu propuesta: "))
+    if (intento == mm_count):
+        chances = -1;
+        print(f"Lo conseguiste! El número correcto era {intento}")
+    else:
+        if (intento > mm_count):
+            print(f"Lo lamento, no es correcto, el número buscado es menor que {intento}")
+            chances -= 1
+        else:
+            print(f"Lo lamento, no es correcto, el número buscado es mayor que {intento}")
+            chances -= 1
+
+if (chances == 0):
+    print(f"GAME OVER: No has sido capaz de encontrar el número. El número correcto era {mm_count}")
+
+
